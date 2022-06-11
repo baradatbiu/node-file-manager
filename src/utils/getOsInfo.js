2581dfdd5880792d3b90f56249ace3c2ad277a8e
Dmitry Kharlamov <newdimaak@gmail.com>
2022-06-11 16:16:21 +0300

    feat: implement os info cli

---
+++ src/utils/getOsInfo.js
@@ -0,0 +1,25 @@
+import { arch, cpus, EOL, homedir, userInfo } from "os";
+import { OS_PROPS } from "../constants.js";
+
+export const getOsInfo = (osInfoKey) => {
+  switch (osInfoKey) {
+    case OS_PROPS.EOL:
+      return JSON.stringify(EOL);
+
+    case OS_PROPS.CPUS:
+      return cpus();
+
+    case OS_PROPS.HOMEDIR:
+      return homedir();
+
+    case OS_PROPS.USERNAME:
+      const { username } = userInfo();
+      return username;
+
+    case OS_PROPS.ARCHITECTURE:
+      return arch();
+
+    default:
+      throw new Error();
+  }
+};
